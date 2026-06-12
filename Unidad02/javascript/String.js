26d6ea98309b972279381e2aa6431d4530b4c271
kleyners22 <kleynerburi33@gmail.com>
2026-06-12 13:09:12 -0500

    Feat: learning Javascript for noobscis

---
+++ Unidad02/javascript/String.js
@@ -0,0 +1,17 @@
+
+let string1 = 'Hola Mundo';
+let string2 = 'Javascript es chevere!';
+let string3 = '$(string1) feliz :)';
+let string4 = string1 + ' ' + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = 'Javascript es Genial'
+console.log (frase.length);
+console.log (frase.toUpperCase());
+console.log (frase.toLowerCase());
+console.log (frase.substring(11,29))
+
---
+++ Unidad02/javascript/Tipos_Datos.js
@@ -0,0 +1,27 @@
+
+//string
+let nombre = 'Maria';
+//numerico
+let edad = 26;
+//booleanos
+let esMayorDeEdad = true
+//null
+let myHayValor = null
+//undefinied
+let noDefinido = undefined;
+//symbol valor unico
+let simboloUnico = sumbol('unico');
+//datos gigantescos bigint
+let numeroGrande = 2n
+//COOMPLETOS
+//object
+let carro = {
+    marca : 'Tesla',
+    modelo : 'Model S',
+}
+let frutas = ('manzana','pera','piña')
+
+//funciones
+function saludar(){
+    'Hola'
+}
