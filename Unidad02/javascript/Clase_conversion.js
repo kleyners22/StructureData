07908595ffa4e7b01e6d998a157d39c41286502c
kleyners22 <kleynerburi33@gmail.com>
2026-06-16 11:37:07 -0500

    feat: clase del 16/06/2026,Nombre: Kleyner Oswaldo Buri Diaz

---
+++ Unidad02/javascript/Clase_conversion.js
@@ -0,0 +1,36 @@
+
+const stringdecimal = '3.14';
+const float = parseFloat(stringdecimal);
+
+console.log(stringdecimal)
+
+const binary = '10010';
+const decimal = parseInt(binary,2);
+
+console.log(decimal)
+
+const suma = 3 - true;
+console.log(suma)
+
+/*
+condicionales
+
+== si es del mismo valor 
+=== si es del mismo tipo y del mismo valor
+!= diferente comparar solo el valor
+!== si es diferente del mismo tipo y del mismo valor
+
+< menor
+> mayor
+
+AND
+&&
+
+
+OR
+||
+
+NOT
+!
+*/
+
---
+++ Unidad02/javascript/Clase_number.js
@@ -0,0 +1,37 @@
+//Tipos de datos numericos
+//enteros
+
+const entero = 52;
+const decimal = 3.14;
+
+console.log(typeof entero,typeof decimal)
+//decimales
+
+//notacion  cientifica
+const cientifico = 5e3;
+
+//representacion de otros contextos
+
+const infinito = Infinity;
+const noEsNumerico = NaN;
+
+//Sumas Restas
+
+const suma = 3+4;
+const resta = 10-5;
+const multiplicacion = 5*4;
+const division = 20/5;
+
+//Resultado de precision
+
+const exponenciacion = 0.1 +0.2 
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(1))
+
+const raizCuadrada = Math.sqrt(16);
+const valorAbsoluto = Math.abs(-7);
+const aleatorio = Math.random();
+
+console.log(raizCuadrada);
+console.log(valorAbsoluto);
+console.log(aleatorio);
